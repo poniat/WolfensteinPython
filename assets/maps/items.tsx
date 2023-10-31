14deb418a97177f109f9ee636772946adae9ba00
poniat <wojciech.poniatowski@gmail.com>
2023-10-31 19:20:22 +0100

    more assets, bigger map

--- assets/maps/items.tsx
+++ assets/maps/items.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="items" tilewidth="897" tileheight="1921" tilecount="21" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="items" tilewidth="897" tileheight="1921" tilecount="39" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="64" height="64" source="../sprites/static/ammo.png"/>
@@ -63,5 +63,59 @@
  </tile>
  <tile id="20">
   <image width="64" height="64" source="../sprites/static/rifle.png"/>
+ </tile>
+ <tile id="21">
+  <image width="64" height="64" source="../sprites/static/waze_blue.png"/>
+ </tile>
+ <tile id="22">
+  <image width="64" height="64" source="../sprites/static/barrel_green.png"/>
+ </tile>
+ <tile id="23">
+  <image width="64" height="64" source="../sprites/static/bed.png"/>
+ </tile>
+ <tile id="24">
+  <image width="64" height="64" source="../sprites/static/cage_with_skeleton.png"/>
+ </tile>
+ <tile id="25">
+  <image width="64" height="64" source="../sprites/static/column.png"/>
+ </tile>
+ <tile id="26">
+  <image width="64" height="64" source="../sprites/static/golden_chalice.png"/>
+ </tile>
+ <tile id="27">
+  <image width="64" height="64" source="../sprites/static/golden_chest.png"/>
+ </tile>
+ <tile id="28">
+  <image width="64" height="64" source="../sprites/static/golden_cross.png"/>
+ </tile>
+ <tile id="29">
+  <image width="64" height="64" source="../sprites/static/golden_crown.png"/>
+ </tile>
+ <tile id="30">
+  <image width="64" height="64" source="../sprites/static/green_standing_lamp.png"/>
+ </tile>
+ <tile id="31">
+  <image width="64" height="64" source="../sprites/static/hanging_skeleton.png"/>
+ </tile>
+ <tile id="32">
+  <image width="64" height="64" source="../sprites/static/hangling_pots.png"/>
+ </tile>
+ <tile id="33">
+  <image width="64" height="64" source="../sprites/static/key_blue.png"/>
+ </tile>
+ <tile id="34">
+  <image width="64" height="64" source="../sprites/static/key_gold.png"/>
+ </tile>
+ <tile id="35">
+  <image width="64" height="64" source="../sprites/static/life_up.png"/>
+ </tile>
+ <tile id="36">
+  <image width="64" height="64" source="../sprites/static/minigun.png"/>
+ </tile>
+ <tile id="37">
+  <image width="64" height="64" source="../sprites/static/sink.png"/>
+ </tile>
+ <tile id="38">
+  <image width="64" height="64" source="../sprites/static/table_with_chairs.png"/>
  </tile>
 </tileset>
