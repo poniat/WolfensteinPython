bb49b3f2f2f6599dd67ad1bdda7cf8608bf2f942
poniat <wojciech.poniatowski@gmail.com>
2023-10-31 14:06:46 +0100

    add more wall textures

---
+++ assets/maps/items.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="items" tilewidth="897" tileheight="1921" tilecount="12" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="../sprites/static/ammo.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../sprites/static/blood_bones.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../sprites/static/bones1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="../sprites/static/bones2.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="../sprites/static/cage.png"/>
+ </tile>
+ <tile id="5">
+  <image width="897" height="1921" source="../sprites/static/candlebra.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="../sprites/static/food.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="../sprites/static/guard_dead.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="../sprites/static/guard_stand.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="../sprites/static/knight-armor.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="../sprites/static/lamp_ceiling.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="../sprites/static/medkit.png"/>
+ </tile>
+</tileset>
---
+++ assets/maps/walls.tsx
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="walls" tilewidth="64" tileheight="64" tilecount="14" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="../textures/wood_wall.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../textures/wood_wall_eagle.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../textures/wood_wall_hitler.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="../textures/blue_wall_brick.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="../textures/wall_1_64.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="../textures/wall_2_64.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="../textures/wall_3_64.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="../textures/wall_4_64.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="../textures/blue_wall_brick_verboten.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="../textures/blue_wall_brick_cage.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="../textures/blue_wall_brick_cage_bones.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="../textures/door_front.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="64" source="../textures/door_left.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="64" source="../textures/door_right.png"/>
+ </tile>
+</tileset>
